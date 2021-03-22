9f804201706e98efd539314ff2ed79169a546505
Jvala2 <joevalancy@gmail.com>
2021-03-22 16:37:59 -0400

    Password now appears in the box.

--- script.js
+++ script.js
@@ -12,25 +12,25 @@ var finalP = []
 var output
 // The function gives a prompt to the user.
 function writePassword() {
-  pLength = prompt("Enter password legth. (8-128 characters");
+  pLength = prompt("How many characters long do you want your password? Enter a number from 8 to 128.");
     if ((pLength >= 8) && (pLength <= 128)) {}
     else {alert("Please enter a valid number."); return}
   //Here it asks how long you want the password. If you enter anything not a number between 8-128, it terminates.
   //pLenght is set as your input
 
-  pCase = prompt("Do you want uppercase? Y/N");
+  pCase = prompt("Do you want random UPPERCASE letters? Y/N");
     if (pCase == "Y") {mArray.push("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");}
     else if (pCase == "N") {}
     else {alert("Invalid response. Please enter Y or N. Remember, input is Case sensitive."); return}
   //If you select Y for uppercase, it pushes the entire upper case input into the mArray for passwords.
 
-  pNumbers = prompt("Do you want numbers? Y/N");
-    if (pNumbers == "Y") {mArray.push("1","2","3","4","5","6","7","8","9","0");}
+  pNumbers = prompt("Do you want random numbers? Y/N");
+    if (pNumbers == "Y") {mArray.push("1","2","3","4","5","6","7","8","9","0","1","2","3","4","5","6","7","8","9","0");}
     else if (pNumbers == "N") {}
     else {alert("Invalid response. Please enter Y or N. Remember, input is Case sensitive."); return}
-  //Same as the Upper Case one, except for numbers. There are less numbers than letters so it gives less numbers than letters.
+  //Same as the Upper Case one, except for numbers. There are less numbers than letters I doubled the numbers to be pushed to the array.
 
-  pChar = prompt("Do you want special characters? Y/N");
+  pChar = prompt("Do you want random special characters? Y/N");
     if (pChar == "Y") {mArray.push("!","@","#","$","%","^","&","*","(",")","-","=","+");}
     else if (pChar == "N") {}
     else {alert("Invalid response. Please enter Y or N. Remember, input is Case sensitive."); return}
@@ -49,8 +49,8 @@ function writePassword() {
   
   output = finalP.join("");
   console.log(output);
-  alert(output);
-  //Finally, this part joins the finalP array into a seemless randomly generated password and outputs an alert.
+  document.getElementById("password").value = output;
+  //Finally, this part joins the finalP array into a seemless randomly generated password and outputs to the page.
 }
 
 // Add event listener to generate button
