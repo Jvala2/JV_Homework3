7ad350598e24ff466d31ef874a892ae56af308d9
Jvala2 <joevalancy@gmail.com>
2021-03-22 12:31:37 -0400

    Removed comments

--- Develop/script.js
+++ Develop/script.js
@@ -1,20 +1,3 @@
-/* GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN prompted for character types to include in the password
-THEN I choose lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
-```
-*/
 
 /* 
 1 Prompt character lenth message 8-128
@@ -54,11 +37,7 @@ function writePassword() {
 
   console.log(mArray) /* Remember to remove this */
 
-  // function getRandomElementFromArray(array) {
-  //   var randomPosition=generateRandomNumber(array.length);
-  //   return array[randomPosition];
-  // }
-  // var lCase=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  
   for(var i=0;i<pLength;i=i+1) {
     var password = mArray[Math.floor(Math.random() * mArray.length)];
     console.log(password)
